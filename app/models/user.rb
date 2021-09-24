eee5bdd260556d101cfb74c83b1c27bac915660d
Naveenr113 <navv6565@gmail.com>
2021-09-24 18:15:29 +0530

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+    has_many :passive_relationships, class_name:  "Relationship",
+                                     foreign_key: "followed_id",
+                                     dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships,source: :follower
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
@@ -35,6 +43,7 @@ end
   def forget
     update_attribute(:remember_digest, nil)
   end
+
   #Activates an account.
     def activate
       update_attribute(:activated,    true)
@@ -62,10 +71,31 @@ end
     def password_reset_expired?
       reset_sent_at < 2.hours.ago
     end
+
   # Defines a proto-feed.
   # see "Following users" for the full implementation.
+  # Returns a user's status feed.
     def feed
-      Micropost.where("user_id = ?", id)
+      following_ids = "SELECT followed_id FROM relationships
+                       WHERE  follower_id = :user_id"
+      Micropost.where("user_id IN (#{following_ids})
+                       OR user_id = :user_id", user_id: id)
+               .includes(:user, image_attachment: :blob)
+    end
+
+  #Follows a user.
+    def follow(other_user)
+      following << other_user unless  self == other_user
+    end
+
+  #Unfollows a user.
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+
+  #Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
     end
 
     private
@@ -78,4 +108,3 @@ end
       self.activation_digest = User.digest(activation_token)
     end
 end                      
-
